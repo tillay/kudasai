c046eeb2c824f5cbc36b890b4070dd177e31db8e
Tilley <tilleyy8@gmail.com>
2025-04-29 22:23:39 -0600

    major changes

--- kudasai.c
+++ kudasai.c
@@ -4,24 +4,30 @@
 #include <unistd.h>
 #include <string.h>
 
-const char *insert_path = NULL, *remove_path = NULL, *rand_dir = NULL;
-void play_random_mp3(const char *dirpath) {
+char *insert_path = NULL, *remove_path = NULL;
+char *base_path = "/usr/share/kudasai/";
+
+void play_mp3_filepath(const char *path) {
     char cmd[256];
-    snprintf(cmd, sizeof(cmd), "play \"%s/$(ls %s | shuf -n 1)\" >/dev/null 2>&1", dirpath, dirpath);
+    snprintf(cmd, sizeof(cmd), "play \"$(find \"%s\" -type f | shuf -n 1)\">/dev/null 2>&1", path);
     system(cmd);
 }
-
-void play_mp3_filepath(const char *filepath) {
+void list_sound_dirs() {
     char cmd[256];
-    snprintf(cmd, sizeof(cmd), "play \"%s\" >/dev/null 2>&1", filepath);
+    printf("Valid options for use with -s (insert/remove pair)\n");
+    snprintf(cmd, sizeof(cmd), "find \"%s\" -mindepth 1 -type d -exec sh -c '[ -f \"$1/insert.mp3\" -a -f \"$1/remove.mp3\" ] && basename \"$1\"' _ {} \\;", base_path);
     system(cmd);
 }
 
 int hotplug_callback(struct libusb_context *ctx, struct libusb_device *dev, libusb_hotplug_event event, void *_) {
-    if (event == LIBUSB_HOTPLUG_EVENT_DEVICE_ARRIVED)
-        use_windows ? play_mp3_filepath("/home/$USER/kudasai/windows/insert.mp3") : insert_path ? play_mp3_filepath(insert_path) : rand_dir ? play_random_mp3(rand_dir) : 0;
-    else if (event == LIBUSB_HOTPLUG_EVENT_DEVICE_LEFT)
-        use_windows ? play_mp3_filepath("/home/$USER/kudasai/windows/remove.mp3") : remove_path ? play_mp3_filepath(remove_path) : rand_dir ? play_random_mp3(rand_dir) : 0;
+
+    if (event == LIBUSB_HOTPLUG_EVENT_DEVICE_ARRIVED){
+        if (insert_path != NULL) play_mp3_filepath(insert_path);
+    }
+    else if (event == LIBUSB_HOTPLUG_EVENT_DEVICE_LEFT) {
+        if (remove_path != NULL) play_mp3_filepath(remove_path);
+    }
+
     return 0;
 }
 
@@ -29,29 +35,50 @@ int main(int argc, char *argv[]) {
     if (argc == 1) {
         printf(
             "Usage: kudasai [OPTION]... [FILE]...\n"
-
-            "For the following:"
-            "A lowercase letter means one singular file."
-            "A capital letter means a directory to pull a random file from."
-            "Absolute or relative file paths are both ok"
-
-            "-f -F <path>   Path for both insertion and removal sounds"
-            "-i -I <path>   Path for USB insertion sound\n"
-            "-r -R <path>   Path for USB removal sound\n"
+            "-f <path>   Path for both insertion and removal sounds\n"
+            "-i <path>   Path for USB insertion sound\n"
+            "-r <path>   Path for USB removal sound\n"
+            "-s <name>   Name of premade audio to use.\n"
+            "-l          List premade audio packs to use with -s\n"
         );
-        return 0;
-    }    
-
-    for (int i = 1; i < argc; i++) {
-        if (!strcmp(argv[i], "-f") && i + 1 < argc) rand_dir = argv[++i];
-        else if (!strcmp(argv[i], "-i") && i + 1 < argc) insert_path = argv[++i];
-        else if (!strcmp(argv[i], "-r") && i + 1 < argc) remove_path = argv[++i];
+        return 1;
     }
 
+    for (int i = 1; i < argc; i++) {
+        char *arg = argv[i];
+    
+        switch (arg[1]) {
+            case 'f':
+                if (++i < argc) insert_path = remove_path = argv[i];
+                else return printf("please add a file path\n"), 1;
+                break;
+            case 'i':
+                if (++i < argc) insert_path = argv[i];
+                else return printf("please add a file path\n"), 1;
+                break;
+            case 'r':
+                if (++i < argc) remove_path = argv[i];
+                else return printf("please add a file path\n"), 1;
+                break;
+            case 's':
+                if (++i < argc) {
+                    asprintf(&insert_path, "%s%s/insert.mp3", base_path, argv[++i]);
+                    asprintf(&remove_path, "%s%s/remove.mp3", base_path, argv[++i]);
+                } else return printf("please add a pack name. run kudasai -l to get options\n"), 1;
+                break;
+            case 'l':
+                list_sound_dirs();
+                return 0;
+            default:
+                return printf("invalid flag!\n"), 1;
+        }
+    }
+    
     libusb_context *ctx = NULL;
     libusb_hotplug_callback_handle cb;
     libusb_init(&ctx);
     libusb_hotplug_register_callback(ctx, LIBUSB_HOTPLUG_EVENT_DEVICE_ARRIVED | LIBUSB_HOTPLUG_EVENT_DEVICE_LEFT, 0, LIBUSB_HOTPLUG_MATCH_ANY, LIBUSB_HOTPLUG_MATCH_ANY, LIBUSB_HOTPLUG_MATCH_ANY, hotplug_callback, NULL, &cb);
     while (1) libusb_handle_events_completed(ctx, NULL);
+
     return 0;
 }
